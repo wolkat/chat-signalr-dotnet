baa8e267eb992613e41c6acb43e27b86038adf59
kwolszon <kwolszon@bitbucket.org>
2015-01-25 13:58:31 +0100

    Updated SeleniumTest

--- ChatServerTest/SeleniumTest.cs
+++ ChatServerTest/SeleniumTest.cs
@@ -15,7 +15,17 @@ namespace ChatServerTest
         public void FirstSeleniumTest()
         {
             var driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            //var driver = new OpenQA.Selenium.Chrome.ChromeDriver();
             driver.Navigate().GoToUrl("http://www.google.pl");
+            Assert.That(driver.Title,Is.EqualTo("Google"));
+            var element = driver.FindElementById("gbqfq");
+            element.SendKeys("google\n");
+            var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var result = wait.Until(d => {
+                return d.PageSource.Contains("Wyszukiwarka stron WWW");
+            });
+            Assert.IsTrue(result);
+            driver.Quit();
         }
     }
 }
